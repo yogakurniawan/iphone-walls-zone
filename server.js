7b201f68bea5334849d8abff1a553fcbddf87e3c
Yoga Kurniawan <yoga.kwn@gmail.com>
2017-12-17 10:41:55 +0700

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url')
 const next = require('next')
 const routes = require('./routes')
 
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = parseInt(process.env.PORT, 10) || 5000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = routes.getRequestHandler(app)
